452ee1ca4bc02730bb530745909dd02643220f8b
summereuna <summereuna@gmail.com>
2022-01-05 00:09:42 +0900

    Add call control buttons

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,34 +1,56 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
+//0. 가져오기
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
-/*
-이제 stream을 받아야 한다. 
-stream은 비디오와 오디오가 결합된 거다 ㅇㅇ!
-
-
-*/
-//1. myStream 전역변수로 만들기
 let myStream;
+//2. 전역변수 만들기
+//음소거 여부를 추적할 수 있는 variable
+//처음에 소리가 나는 상태로 시작하기 때문에 디폴트로 false를 준다.
+let muted = false;
+//카메라 on/off 여부를 추적할 수 있는 variable
+//처음에 화면이 켜진 상태로 시작하기 때문에 디폴트로 false를 준다.
+let cameraOff = false;
 
-//2. 함수 생성
 async function getMedia() {
   try {
-    //2-1. navigator.mediaDevices.getUserMedia(constraints);
-    //는 유저의 유저미디어 string을 준다.
-    //constraints은 기본적으로 우리가 무엇을 얻고 싶은지이다. 오디오랑, 비디오!
     myStream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: true,
     });
-    //2-2.되는지 확인
-    //console.log(myStream);
-    //2-3. myStream을 video인 myFace 안에 넣어주자
     myFace.srcObject = myStream;
   } catch (err) {
     console.log(err);
   }
 }
 
-//3. 펑션 호출
 getMedia();
+
+//3. 핸들러에 전역변수 적용해서 버튼 클릭 시 바뀌게 해주기
+function handleMuteClick() {
+  console.log(myStream.getAudioTracks());
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+function handleCameraClick() {
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = true;
+  }
+}
+
+//1. 버튼에 이벤트 추가
+//mute 버튼 클릭 시 음소거되고, 텍스트 Unmute로 바꾸기
+//camera 버튼 클릭 시 카메라 끄고, 텍스트 Turn Camera On으로 바꾸기
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
