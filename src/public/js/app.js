e584c592b5e91ea03d7de13948ab9592ef63f469
summereuna <summereuna@gmail.com>
2022-01-06 00:39:53 +0900

    Add camera switch fn

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,36 +1,80 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
-//0. 가져오기
-const muteBtn = document.getElementById("mute");
-const cameraBtn = document.getElementById("camera");
+const muteBtn = document.getElementById("muteBtn");
+const cameraBtn = document.getElementById("cameraBtn");
+const camerasSelect = document.getElementById("cameras");
 
+//전역 변수
 let myStream;
-//2. 전역변수 만들기
-//음소거 여부를 추적할 수 있는 variable
-//처음에 소리가 나는 상태로 시작하기 때문에 디폴트로 false를 준다.
 let muted = false;
-//카메라 on/off 여부를 추적할 수 있는 variable
-//처음에 화면이 켜진 상태로 시작하기 때문에 디폴트로 false를 준다.
 let cameraOff = false;
 
-async function getMedia() {
+//사용자의 카메라 장치 가져오는 함수
+async function getCamera() {
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    //device의 kind가 "videoinput"인 device만 필터링해서 가져오기
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    //현재 선택된 카메라, 즉 어플리케이션 처음 들어갈 때 선택되어 있는 카메라
+    const currentCamera = myStream.getVideoTracks()[0];
+    //카메라스 안에 있는 각각의 카메라 아이템에게 적용 해라~
+    cameras.forEach((camera) => {
+      //새로운 옵션 생성
+      const option = document.createElement("option");
+      //옵션의 밸류 값으로 각 camera의 deviceId 를 넣어주자.
+      option.value = camera.deviceId;
+      //옵션의 text 값으로 각 camera의 label을 넣어주자.
+      option.innerText = camera.label;
+      //현재 카메라의 label이 cameras에 있는 label과 같다면 그 label이 선택되게하자.
+      //이 함수는 어플리케이션 처음 시작할 때만 실행됨
+      //이렇게 하면 stream의 현재 카메라와 paint할 때의 카메라 옵션이 같게 잘 나온다.
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      //만들어진 옵션을 camerasSelect 안에 넣어주면 된다.
+      camerasSelect.appendChild(option);
     });
-    myFace.srcObject = myStream;
   } catch (err) {
     console.log(err);
   }
 }
 
+//stream 가져오기(비디오, 오디오)
+//argument로 deviceId 보내기
+async function getMedia(deviceId) {
+  //처음에 devicdId 없이 getMedia가 호출되기 때문에 이에 대한 constraints 설정을 전면카메라(셀피) 우선으로 해주자.
+  const initialConstraints = { audio: true, video: { facingMode: "user" } };
+  //셀렉트 후 deviceId가 있는 채로 getMedia가 호출될 때
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+  try {
+    //삼항연산자 사용해서 deviceId 있는 경우와 없는 경우 사용할 Constraints 설정하기
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstraints
+    );
+    myFace.srcObject = myStream;
+    //Media를 get한 후, getCamera 호출
+    //내가 가진 카메라를 옵션으로 페인팅해주는 getCamera를 deviceId가 없을 때 즉, 첨 딱 한번만 실행되게 해주기
+    if (!deviceId) {
+      await getCamera();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//stream 호출하기
 getMedia();
 
-//3. 핸들러에 전역변수 적용해서 버튼 클릭 시 바뀌게 해주기
+//muteBtn 클릭 시 작동하는 핸들러
 function handleMuteClick() {
-  console.log(myStream.getAudioTracks());
+  //버튼 클릭 시 오디오 track에 track.enabled을 현재 값과 반대 값이 되게 하기
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -39,18 +83,29 @@ function handleMuteClick() {
     muted = false;
   }
 }
+
+//cameraBtn 클릭 시 작동하는 핸들러
 function handleCameraClick() {
-  if (cameraOff) {
+  //버튼 클릭 시 비디오 track에 track.enabled을 현재 값과 반대 값이 되게 하기
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!cameraOff) {
     cameraBtn.innerText = "Turn Camera On";
-    cameraOff = false;
+    cameraOff = true;
   } else {
     cameraBtn.innerText = "Turn Camera Off";
-    cameraOff = true;
+    cameraOff = false;
   }
 }
 
-//1. 버튼에 이벤트 추가
-//mute 버튼 클릭 시 음소거되고, 텍스트 Unmute로 바꾸기
-//camera 버튼 클릭 시 카메라 끄고, 텍스트 Turn Camera On으로 바꾸기
+//카메라 선택 바꾸면 작동하는 핸들러: 각 카메라 deviceId 사용해 stream 바꿔주기
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
+//컨트롤 버튼에 이벤트 추가
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+//카메라 선택하면 이벤트 추가
+camerasSelect.addEventListener("iput", handleCameraChange);
